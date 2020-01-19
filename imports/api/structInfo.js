4d1b6187bfac36b916b96434cadec5e994fe0c36
Daniel Cagua Ennis <d.cagua10@uniandes.edu.co>
2020-01-19 11:21:38 -0500

    Update Market Services
    -Llega hasta la asignacion de servicios a cada mercado

--- imports/api/structInfo.js
+++ imports/api/structInfo.js
@@ -35,6 +35,44 @@ exports.createMercado = function (localProyectoId,localMercadoName)
         mercado_name: localMercadoName,
         servicio_list: []
     }
+    return nuevoMercado;
 }
 
+exports.addMarketServices = function (listaGenObjServicios,listaGenObjMercados)
+{
+      console.log("P1: ", listaGenObjServicios)
+      console.log("P2: ", listaGenObjMercados)
+
+      var i,j;
+      //var serviciosListMercadoAct = [];
+      //var nuevaListaMercados=[];
+
+      var copiaListaServicios = listaGenObjServicios;
+      var copiaListaMercados = listaGenObjMercados;
+      var mercadoActualAdd;
+      var nuevaListaMercados=[];
+
+      for (i=0; i<copiaListaMercados.length; i++)
+      {
+        var actualMercado = copiaListaMercados[i];
+        var actualMercadoPID = actualMercado.proyecto_id;
+
+        for(j=0; j<copiaListaServicios.length; j++)
+        {
+          var actualServicio = copiaListaServicios[j];
+          var actualServicioPID = actualServicio.proyecto_id
+
+          if(actualServicioPID===actualMercadoPID)
+          {
+            console.log("son iguales")
+            console.log(i,"-",j,"-",actualMercado,"-",actualServicio)
+            var mercadoActualAdd = actualMercado.servicio_list;
+            mercadoActualAdd.push(actualServicio)
+            nuevaListaMercados=copiaListaMercados
+          }
+        }
+      }
+      console.log("LM: ",nuevaListaMercados)
+      return nuevaListaMercados;
+}
 
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -38,7 +38,7 @@ var listaTareasVOLVO = [];
 //Estructuras Generales
 var servicioObject;
 var mercadoObject;
-var empresaProyect;
+var empresaObject;
 
 //Listas Generales
 var listaServiciosDisponibles=[];
@@ -63,9 +63,9 @@ class App extends Component {
 
       /*Testing de Metodos*/
       //console.log(Services.getProjects())
-      console.log("--------------")
+      //console.log("--------------")
       //console.log(Services.getUsers())
-      console.log("--------------")
+      //console.log("--------------")
       //console.log(Services.getTasksByProjectId("2582841","1753169","1753169"))
       //projectId, assignedId, createdById, pDueDate
       //console.log("--------------")
@@ -74,6 +74,7 @@ class App extends Component {
 
     getAllEmpresas(callback)
     {
+      console.log("getAllEmpresas")
       var projects = Services.getProjects()
         .then((response) => response.json())
           .then((responseJson) => {
@@ -96,7 +97,7 @@ class App extends Component {
       //console.log("Esto me dio el callback: ",projectIdList); 
       //console.log(anotherCallback)
       //console.log("Test Get Tasks by Project ID: ",Services.getTasksByProjectId("2582841","1753169","1753169"))
-
+      console.log("createServices")
       projectIdList.forEach(id => {
         var tasks = Services.getTasksByProjectId(id,userId,userId)
           .then((response) => response.json())
@@ -181,20 +182,13 @@ class App extends Component {
                 servicioObject = null;
               }
 
-              listaGenObjServicios.push(servicioObject)
-
-              //Sacar info goes here
-              //servicioName.push(res.title)
-              //ERROR AL AGREGAR TAREAS
-              //tareasList.push(res.description)
-              //console.log("ADD TAREA: ", res.description) 
-              //console.log("TareasListState: ",tareasList)   
+              listaGenObjServicios.push(servicioObject)   
                     
             });
           }).then(res=>{
-            console.log("PI: ",listaGenObjServicios)
+            //console.log("PI: ",listaGenObjServicios)
             //console.log("inicia el otro callback")
-            anotherCallback(projectIdList, listaGenObjServicios)
+            anotherCallback(projectIdList,listaGenObjServicios, structInfo.addMarketServices)
             
           })
           .catch((error) =>{
@@ -204,69 +198,74 @@ class App extends Component {
       })
     }
 
-    createMarkets(projectIdList, listaGeneralServicios)
+    createMarkets(projectIdList, listaGeneralServicios, callback)
     {
+      console.log("createMarkets")
       projectIdList.forEach(id => {
         var tasks = Services.getTasksByProjectId(id,userId,userId)
           .then((response) => response.json())
           .then((responseJson) => {
             var localResponse;
             localResponse = responseJson.data.tasks
-            console.log("TASK For Markets: ",localResponse)
+            //console.log("TASK For Markets: ",localResponse)
             localResponse.forEach(res => {
-              console.log("mercadoName: ",res.task_group_name,"-","servicioName: ",res.title,"-","tareas: ",res.description)
+              //console.log("mercadoName: ",res.task_group_name,"-","servicioName: ",res.title,"-","tareas: ",res.description)
               var localProyectoId = res.project_id;
               var localMercadoName = res.task_group_name;
               
               var booleanListMercados;  
 
               booleanListMercados = listaMercadosDisponibles.includes(localMercadoName);
+              //console.log("Bool State: ", booleanListMercados)
               if(booleanListMercados==false)
               {
                 listaMercadosDisponibles.push(localMercadoName);
                 mercadoObject=structInfo.createMercado(localProyectoId,localMercadoName)
-
+                listaGenObjMercados.push(mercadoObject)   
               }
-                listaGenObjMercados.push(mercadoObject)              
+                          
             });
           }).then(res=>{
+            //Añadir los servicios a los mercados
+            console.log("ListaGenServicios: ",listaGeneralServicios)
             console.log("ListaGenMercados: ",listaGenObjMercados)
-            //console.log("inicia el otro callback")
-            //anotherCallback(listaGenObjServicios)
-            
+
+            var listaGS = listaGeneralServicios;
+            var listaGM = listaGenObjMercados;
+
+            var pListaGM=callback(listaGS,listaGM)
+            listaGenObjMercados=pListaGM       
           })
           .catch((error) =>{
             console.error(error);
           });
          
       })
-      /*//console.log("Entro al procesamiento!")
-      var i;
-      //console.log("ParametersProcess: ",serviciosList,"-".tareasList)
-      for(i=0; i<projectId.length; i++)
-      {
-        var tareas = tareasList[i];
-        var servicios = {
-          servicio_name: serviciosList[i],
-          tareas_list: tareas
-        }
-        var mercados = {
-          mercado_name: projectDescription[i],
-          servicios_list: servicios
-        }
-        var empresa =
-        {
-          project_id: projectId[i],
-          project_name: projectName[i],
-          mercados_list: mercados,
-        }
-
-        //console.log("Empresa:",empresa)
-        //Agrega el archivo a la base de datos
-        //Meteor.call("empresas.add",empresa)
-      }*/
-
     }
+    
+    addMarketServices(){
+      console.log("Socio")
+    }
+      /*var i,j;
+      var nuevaListaMercados;
+      for (i=0; i<listaGenObjMercados.length; i++)
+      {
+        var actualMercado = listaGenObjMercados[i];
+        for(j=0; j<listaGenObjServicios.length; i++)
+        {
+          var actualServicio = listaGenObjServicios[j];
+          var actualMercadoPID = actualMercado.proyecto_id;
+          var actualServicioPID = actualServicio.proyecto_id
+          
+
+        }
+      }
+
+      //console.log("inicia el otro callback")
+      //anotherCallback(listaGenObjServicios)
+    }*/
+
+
     render(){
         return (
             <div>
